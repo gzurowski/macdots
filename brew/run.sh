8b2e9f2e65137013d677bf9a1c3e21faaa888b7a
Gregor Zurowski <gregor@zurowski.net>
2023-03-08 19:17:42 +0100

    Add support for Silicon/Mac when running Homebrew setup

--- brew/run.sh
+++ brew/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 BREWFILE="$(dirname $0)/Brewfile"
-BREW_PATH="/opt/homebrew"
+BREW_PATH="/opt/homebrew" # Apple Silicon
+if test "$(uname -m)" = "x86_64"; then
+    BREW_PATH="/usr/local" # Intel
+fi
 
 # Install Homebrew
 if ! test -d "$BREW_PATH"; then
@@ -9,7 +12,8 @@ if ! test -d "$BREW_PATH"; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if test -d "$BREW_PATH"; then
+# Set Homebrew environment if not already set
+if test -d "$BREW_PATH" && test ! $(which brew); then
     eval "$("${BREW_PATH}"/bin/brew shellenv)"
 fi
 
