88cca1855c87534c7cfc7b0880cc6f0ecf1fdb1e
Gregor Zurowski <gregor@zurowski.net>
2023-03-08 13:25:17 +0100

    Improve check for existing Homebrew installation

--- brew/run.sh
+++ brew/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 BREWFILE="$(dirname $0)/Brewfile"
+BREW_PATH="/opt/homebrew"
 
 # Install Homebrew
-if test ! $(which brew); then
+if ! test -d "$BREW_PATH"; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval "$(/opt/homebrew/bin/brew shellenv)"
