e18ddfeccb58da59b8913c4f53c8842a9226ecf5
Gregor Zurowski <gregor@zurowski.net>
2023-03-08 13:14:20 +0100

    Add Homebrew's zsh to shell
    Any shell that is made the default shell via `chsh` needs to be
    listed in `/etc/shells`.

--- zsh/run.sh
+++ zsh/run.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -ex
+# Path to Homebrew's zsh
+ZSH_PATH="/usr/local/bin/zsh"
 
 # Install oh-my-zsh if not present
 if test -z "${ZSH}" || test ! -d "${ZSH}"; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 fi
 
+# Add Homebrew's zsh to /etc/shells, so it can be made the default shell
+if ! grep -q "$ZSH_PATH" /etc/shells; then
+    echo "$ZSH_PATH" | sudo tee -a /etc/shells
+fi
+
 # Make Homebrew's zsh the default shell
-chsh -s /usr/local/bin/zsh
+chsh -s $ZSH_PATH
