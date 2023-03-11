fbb4e1889f3dd94ba2f2134d76bb049040bb0030
Gregor Zurowski <gregor@zurowski.net>
2023-03-11 18:59:33 +0100

    Enable Touch ID on Silicon Macs

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: brew git gpg zsh
+.PHONY: brew git gpg mac zsh
 
 brew:
 	@./brew/run.sh
@@ -8,6 +8,9 @@ git:
 
 gpg: brew
 	@./gpg/run.sh
+
+mac:
+	@./mac/run.sh
 
 zsh: brew
 	@./zsh/run.sh
---
+++ mac/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+# Enable Touch ID for sudo on Silicon Macs
+if test "$(uname -m)" = "arm64" && \
+    ! grep -Eq "auth\s+sufficient\s+pam_tid.so" /etc/pam.d/sudo
+then
+    echo "Enabling Touch ID for sudo..."
+    sudo sed -i '.bak' "2s/^/# Enable Touch ID for sudo:\nauth       sufficient     pam_tid.so\n/" /etc/pam.d/sudo
+fi
