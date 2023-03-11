73911755d535d8974572b7aa2fc10d4d2a62981b
Gregor Zurowski <gregor@zurowski.net>
2023-03-11 16:57:22 +0100

    Add initial GPG setup

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-.PHONY: brew git zsh
+.PHONY: brew git gpg zsh
 
 brew:
 	@./brew/run.sh
 
 git:
 	@./git/run.sh
+
+gpg: brew
+	@./gpg/run.sh
 
 zsh: brew
 	@./zsh/run.sh
---
+++ gpg/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+PINENTRY_CONFIG="pinentry-program $(brew --prefix)/bin/pinentry-mac"
+
+mkdir -p "${HOME}/.gnupg"
+
+# Add pinentry-mac to gpg-agent.conf
+if ! test -f "${HOME}/.gnupg/gpg-agent.conf" || ! grep -q "${PINENTRY_CONFIG}" "${HOME}/.gnupg/gpg-agent.conf"; then
+    echo "Configuring pinentry-mac..."
+    echo "${PINENTRY_CONFIG}" | sudo tee -a "${HOME}/.gnupg/gpg-agent.conf"
+fi
