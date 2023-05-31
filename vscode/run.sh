4b60b5f63fecc11f57d0a0b0256de10592a15b61
Gregor Zurowski <gregor@zurowski.org>
2023-05-31 12:42:11 +0200

    Fix directories in vscode setup

--- vscode/run.sh
+++ vscode/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 BASEDIR=$(dirname "$(readlink -f "$0")")
-EXTENSIONS_FILE="${BASEDIR}/vscode/extensions.txt"
+EXTENSIONS_FILE="${BASEDIR}/extensions.txt"
 
 # Create symlinks
-ln -sfv "${BASEDIR}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User"
+ln -sfv "${BASEDIR}/settings.json" "${HOME}/Library/Application Support/Code/User"
 
 # Install extensions
 while read -r EXTENSION; do
