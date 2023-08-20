60ce85039b94cb01407c296e7d245dbe62169e22
Gregor Zurowski <gregor@zurowski.org>
2023-08-20 17:46:29 +0200

    Lock down `.gnupg` directory

--- gpg/run.sh
+++ gpg/run.sh
@@ -3,6 +3,7 @@ set -e
 PINENTRY_CONFIG="pinentry-program $(brew --prefix)/bin/pinentry-mac"
 
 mkdir -p "${HOME}/.gnupg"
+chmod 700 "${HOME}/.gnupg"
 
 # Add pinentry-mac to gpg-agent.conf
 if ! test -f "${HOME}/.gnupg/gpg-agent.conf" || ! grep -q "${PINENTRY_CONFIG}" "${HOME}/.gnupg/gpg-agent.conf"; then
