9226ffd95081997f3d2d5093febf9b1c3dd3a40b
Gregor Zurowski <gregor@zurowski.org>
2025-03-15 19:25:08 +0100

    Remove iterm2

--- iterm2/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-BASEDIR=$(dirname "$(readlink -f "$0")")
-
-# Set the iTerm2 preferences directory.
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${BASEDIR}"
-
-# Enable iTerm2 to use the custom preference directory.
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
