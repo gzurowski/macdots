b785ddb1bf457404518a5a673aa2cb7abbdceb16
Gregor Zurowski <gregor@zurowski.org>
2025-11-22 13:22:14 +0100

    Add LaunchAgent configuration for organize

--- organize/run.sh
+++ organize/run.sh
@@ -3,4 +3,17 @@ set -e
 BASEDIR=$(dirname "$(readlink -f "$0")")
 
 # Create symlinks
-ln -sfv "${BASEDIR}/config.yaml" "${HOME}/Library/Application Support/organize"
+mkdir -p "${HOME}/.config/organize"
+ln -sfv "${BASEDIR}/config.yaml" "${HOME}/.config/organize/"
+
+# Install LaunchAgent
+ln -sfv "${BASEDIR}/org.zurowski.organize.plist" "${HOME}/Library/LaunchAgents/org.zurowski.organize.plist"
+
+# Load the LaunchAgent
+SERVICE_NAME="org.zurowski.organize"
+if ! launchctl list | grep -q "$SERVICE_NAME"; then
+    echo "Loading $SERVICE_NAME..."
+    launchctl bootstrap gui/"$(id -u)" ~/Library/LaunchAgents/${SERVICE_NAME}.plist
+else
+    echo "$SERVICE_NAME already loaded"
+fi
