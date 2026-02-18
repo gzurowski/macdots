f0d54253e418d5b7cbd8aba9efaa723fa6730f6d
Gregor Zurowski <gregor@zurowski.org>
2026-02-18 10:59:02 +0100

    Disable warning on about:config page

--- firefox/user.js
+++ firefox/user.js
@@ -2,6 +2,9 @@
 //
 // See: https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data
 
+// Disable warning on about:config page
+user_pref("browser.aboutConfig.showWarning", false);
+
 // Show close button on narrow tabs (default is 140)
 user_pref("browser.tabs.tabClipWidth", 40);
 
